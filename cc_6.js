71300063df737ee3d8fee4e30c855a633dd52fbf
Riley Hedges <rhedges1@usf.edu>
2025-02-05 20:15:28 -0500

    Task 3 - Employee Bonus Calculation

--- cc_6.js
+++ cc_6.js
@@ -32,3 +32,29 @@ console.log(`Sales Tax: $${calculateSalesTax(500, .05)}`);
 
 console.log(`Sales Tax: $${calculateSalesTax(1000, .25)}`);
 
+/////////////////////////////
+// Task 3: Arrow Function ///
+/////////////////////////////
+
+console.log("--------------------------------------");
+console.log("Task 3: Arrow Function");
+
+const calculateBonus = (salary, performanceRating) => {
+    let bonusRate = 0;
+    switch (performanceRating) {
+        case "Excellent":
+            bonusRate = .2;
+            break;
+        case "Good":
+            bonusRate = .1;
+            break;
+        case "Average":
+            bonusRate = .05;
+            break;
+    }
+    return salary * bonusRate;
+}
+
+console.log(`Bonus: $${calculateBonus(10000, "Excellent")}`);
+//Another example
+console.log(`Bonus: $${calculateBonus(1000, "Average")}`);
