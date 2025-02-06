6cb74a0a2e1c0c4630b9ffd47413a01b575e2e05
Riley Hedges <rhedges1@usf.edu>
2025-02-05 19:55:55 -0500

    Task 2 - Sales Tax Computation

--- cc_6.js
+++ cc_6.js
@@ -13,3 +13,22 @@ console.log(`Total Profit: $${calculateProfit(100,200,800)}`)
 
 // Another Example
 console.log(`Total Profit: $${calculateProfit(1.25,4.99,1500)}`)
+
+//////////////////////////////////
+// Task 2: Function Expression ///
+//////////////////////////////////
+
+console.log("--------------------------------------");
+console.log("Task 2: Function Expression");
+
+const calculateSalesTax  = function(amount, taxRate) {
+    return amount * taxRate;
+};
+
+
+console.log(`Sales Tax: $${calculateSalesTax(500, .05)}`);
+
+//Another Example
+
+console.log(`Sales Tax: $${calculateSalesTax(1000, .25)}`);
+
